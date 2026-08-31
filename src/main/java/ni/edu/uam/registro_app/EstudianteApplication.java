50fb2ffc28064d8e19acc3aaa357a2e87b946559
RomanVark <jrromero@uamv.edu.ni>
2026-08-30 23:48:23 -0600

    Espero que funcione

--- src/main/java/ni/edu/uam/registro_app/EstudianteApplication.java
+++ src/main/java/ni/edu/uam/registro_app/EstudianteApplication.java
@@ -8,9 +8,9 @@ import javafx.stage.Stage;
 public class EstudianteApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("estudiante-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(EstudianteApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Registro App!");
+        stage.setTitle("Inicio de sesión");
         stage.setScene(scene);
         stage.show();
 
